f2b2696923be51cbbafb20effdb59729e325963f
David-Zhang-test <1839476290@qq.com>
2020-03-26 20:54:45 +0800

    Create jQuery.js

---
+++ jQuery.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(document).ready(function(){
+	$("#pp1").fadeIn(2000)
+});
+$(document).ready(function(){
+	$(".paragraphtwo").mouseover(function(){
+		$("#pp2").fadeIn(1500)
+	});
+});
+$(document).ready(function(){
+	$("#img1").fadeIn(2000)
+});
